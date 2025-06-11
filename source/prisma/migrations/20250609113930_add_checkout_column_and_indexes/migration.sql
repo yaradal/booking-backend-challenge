097052e31b6f3002936cef4a08793296113302ad
yara <y.dallasheh@gmail.com>
2025-06-11 12:06:33 +0200

    fix formatting

--- source/prisma/migrations/20250609113930_add_checkout_column_and_indexes/migration.sql
+++ source/prisma/migrations/20250609113930_add_checkout_column_and_indexes/migration.sql
@@ -1,12 +1,13 @@
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_Booking" (
-                               "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                               "guestName" TEXT NOT NULL,
-                               "unitID" TEXT NOT NULL,
-                               "checkInDate" DATETIME NOT NULL,
-                               "numberOfNights" INTEGER NOT NULL,
-                               "checkOutDate" DATETIME NOT NULL
+CREATE TABLE "new_Booking"
+(
+    "id"             INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "guestName"      TEXT     NOT NULL,
+    "unitID"         TEXT     NOT NULL,
+    "checkInDate"    DATETIME NOT NULL,
+    "numberOfNights" INTEGER  NOT NULL,
+    "checkOutDate"   DATETIME NOT NULL
 );
 INSERT INTO "new_Booking" ("checkInDate", "guestName", "id", "numberOfNights", "unitID", "checkOutDate")
 SELECT "checkInDate", "guestName", "id", "numberOfNights", "unitID",
@@ -17,4 +18,4 @@ ALTER TABLE "new_Booking" RENAME TO "Booking";
 CREATE INDEX "idx_booking_unit_dates" ON "Booking"("unitID", "checkInDate", "checkOutDate");
 CREATE INDEX "idx_booking_guest" ON "Booking"("guestName", "checkInDate", "checkOutDate");
 PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+PRAGMA foreign_keys=ON ;
